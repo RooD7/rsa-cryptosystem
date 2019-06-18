7ade3de2a2c2c00df750c7a488b5139ea2a3dd94
RooD7 <rodrigosoualves7@gmail.com>
2019-06-18 20:26:18 -0300

    Finish RSA

--- Main.java
+++ Main.java
@@ -2,43 +2,53 @@ import java.util.*;
 import java.security.SecureRandom;
 import java.math.BigInteger;
 
-// Gerador de numeros randomicos criptograficamente fortes
-SecureRandom r = new SecureRandom();
-String msg = "Paz e felicidade a todos!";
-String msgcifrada = null;
-String msgdecifrada = null;
-BigInteger n;
-int bitlen = 2048;
 
-public static void main(String[] args) {
+public class Main {
+	public static void main(String[] args) {
 
-	// Geracao de 2 numeros primos grandes
-	BigInteger p = new BigInteger(bitlen / 2, 100, r);
-	BigInteger q = new BigInteger(bitlen / 2, 100, r);
-	
-	// Computa n = p * q
-	n = p.multiply(q);
+		// Gerador de numeros randomicos criptograficamente fortes
+		SecureRandom r = new SecureRandom();
+		String msg = "Paz e felicidade a todos!";
+		String msgcifrada = null;
+		String msgdecifrada = null;
+		BigInteger n;
+		int bitlen = 2048;
 
-	//Computa a funcao totiente m = (p-1)(q-1)
-	BigInteger m = (p.subtract(BigInteger.ONE))
-					.multiply(q.subtract(BigInteger.ONE))
+		// Geracao de 2 numeros primos grandes
+		BigInteger p = new BigInteger(bitlen / 2, 100, r);
+		BigInteger q = new BigInteger(bitlen / 2, 100, r);
+		
+		// Computa n = p * q
+		n = p.multiply(q);
 
-	// Escolhe um inteiro 'e', onde 
-	// 		1 < 'e' < 'm', 
-	//		'e' e 'm' sejam primos entre si
-	BigInteger e = new BigInteger("3");
-	// gcd = maior divisor comum
-	while(m.gcd(e).intValue() > 1)
-		e = e.add(new BigInteger("2"));
+		//Computa a funcao totiente m = (p-1)(q-1)
+		BigInteger m = (p.subtract(BigInteger.ONE))
+						.multiply(q.subtract(BigInteger.ONE));
 
-	// 'd' seja o inverso multiplicativo de 'e'
-	BigInteger d = e.modInverse(m);
+		// Escolhe um inteiro 'e', onde 
+		// 		1 < 'e' < 'm', 
+		//		'e' e 'm' sejam primos entre si
+		BigInteger e = new BigInteger("3");
+		
+		// gcd = maior divisor comum
+		while(m.gcd(e).intValue() > 1)
+			e = e.add(new BigInteger("2"));
 
-	System.out.println("p:"+p);
-    System.out.println("q:"+q);
-    System.out.println("n:"+n);
-    System.out.println("e:"+e);
-    System.out.println("d:"+d);
+		// 'd' seja o inverso multiplicativo de 'e'
+		BigInteger d = e.modInverse(m);
+
+		System.out.println("p:"+p);
+		System.out.println("q:"+q);
+		System.out.println("n:"+n);
+		System.out.println("e:"+e);
+		System.out.println("d:"+d);
+
+		//mensagem cifrada - RSA_encrypt()
+        msgcifrada = new BigInteger(msg.getBytes()).modPow(e, n).toString();
+        System.out.println("msg cifrada: "+ msgcifrada);
+
+        //mensagem decifrada - RSA_decrypt()
+        msgdecifrada = new String(new BigInteger(msgcifrada).modPow(d, n).toByteArray());
+        System.out.println("msg decifrada: " +msgdecifrada);
+	}
 }
-
-public 
